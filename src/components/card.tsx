b957a4a49f2deff9903b6cc90ace1072401397c4
Manuel2u <edodoo011@st.ug.edu.gh>
2023-07-13 00:25:36 +0000

    added increase and decrease buttons

---
+++ src/components/card.tsx
@@ -0,0 +1,35 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+
+const Card = () => {
+  let [weight, setWeight] = useState(0);
+
+  const handleIncrease = () => {
+    setWeight(weight++);
+  };
+
+  const handleDecrease = () => {
+    setWeight(weight--);
+  };
+
+  return (
+    <View className="h-48 w-36  rounded-lg mx-3 bg-primary  flex pt-6 items-center">
+      <Text className="text-white text-2xl font-medium">WEIGHT</Text>
+      <Text className="text-white text-3xl font-medium pt-6">{weight}</Text>
+      <View className="flex pt-6 flex-row gap-x-4">
+        <TouchableOpacity onPress={handleDecrease}>
+          <View className="rounded-full bg-white w-9 h-9 flex items-center">
+            <Text className="text-primary text-3xl  font-bold">-</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleIncrease}>
+          <View className="rounded-full bg-white w-9 h-9 flex items-center">
+            <Text className="text-primary text-3xl  font-bold">+</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Card;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Icons from "../components/icons";
 import { Foundation } from "@expo/vector-icons";
+import Slider from "@react-native-community/slider";
+import Card from "../components/card";
 
 const Home = () => {
+  const handleSubmit = () => {
+    console.log("hello");
+  };
   return (
     <SafeAreaView className="flex-1">
       <View className="flex justify-center items-center">
@@ -31,10 +36,41 @@ const Home = () => {
         />
       </View>
 
-      <View className="flex items-center mt-20 bg-primary h-44 rounded-lg mx-16 pt-10">
+      <View className="flex justify-between  items-center mt-6 bg-primary h-44 rounded-lg mx-14 pt-10">
         <Text className="text-2xl text-white">
           HEIGHT<Text className="text-sm">cm</Text>
         </Text>
+        <Slider
+          style={{ width: 200, height: 40, marginBottom: 20 }}
+          minimumValue={0}
+          maximumValue={1}
+          minimumTrackTintColor="#FF0000"
+          maximumTrackTintColor="#000000"
+        />
+      </View>
+
+      <View className="flex flex-row rounded-lg mx-12 pt-10">
+        <Card />
+        <Card />
+      </View>
+
+      <View
+        className="px-10 pt-5"
+        style={{ alignItems: "center", marginTop: 20 }}
+      >
+        <TouchableOpacity
+          className="w-full bg-primary"
+          onPress={handleSubmit}
+          style={{
+            height: 60,
+            borderRadius: 5,
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 20, color: "white" }}>Calculate</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
