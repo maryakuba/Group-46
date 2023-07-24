30f49b6e54455df645108e4a817d9800b604b171
Lartey Solomon <93642216+GeekYbruCe@users.noreply.github.com>
2023-07-24 15:47:37 +0000

    Update webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,9 @@
 import createExpoWebpackConfigAsync from '@expo/webpack-config/webpack';
 import { Arguments, Environment } from '@expo/webpack-config/webpack/types';
 
+// Define the module.exports function. This function will be called by Expo when it builds your app.
 module.exports = async function (env: Environment, argv: Arguments) {
+  // Call the createExpoWebpackConfigAsync function to get the default Expo webpack config.
   const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it. 
   return config; 
