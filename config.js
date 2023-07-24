f37f6fed85f12194ef88d94cedaea266d5d60a4d
maryakuba <102930626+maryakuba@users.noreply.github.com>
2023-07-24 19:45:30 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+
+      <script src="bundles/ios-87b94ac7284b6e482842ebaf945f5658.js"></script>
+      <script src="bundles/android-a2547f589f346c6a831adab92bc30b8c.js"></script>
+      Open up this file in Google Chrome. In the JavaScript developer console, navigate to the Source tab.
+      You can see a red colored folder containing the original source code from your bundle.
+      
